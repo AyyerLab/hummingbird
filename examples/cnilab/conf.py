2e2db279296b93e66ddb4a16a4e3f6b819deae57
TammeWollweber <tamme.wollweber@gmx.de>
2020-12-07 15:14:36 +0100

    dark corr yes no

--- examples/cnilab/conf.py
+++ examples/cnilab/conf.py
@@ -19,24 +19,26 @@ state['Facility'] = 'CNILab'
 state['CNILab'] = {
     #'Repetition Rate' : 10,
     #'SenderIP': '127.0.0.1',
-    #'SenderIP': '131.169.116.105',
-    'SenderIP': '131.169.224.229', #office pc
+    'SenderIP': '131.169.116.105',
+    #'SenderIP': '131.169.224.229', #office pc
     'SenderPort': 5678,
 }
 
 if state['CNILab']['SenderIP'] != '131.169.116.105':
     name = 'test'
 else:
-    name = 'iStar-cCMOS18F73-S'
+    name = 'iStar-sCMOS18F73-S'
 
 data_sum = None
-dark = []
-dpath = '/home/wittetam/CNIDetector/data/raw/'
-fname = 'Run22_0000.h5'
-with h5.File(dpath+fname, 'r') as f:
-    for key in f:
-        dark.append(np.array(f[key]))
-dark = np.mean(np.array(dark), axis=0)
+do_dark_correction = False
+if do_dark_correction:
+    dark = []
+    dpath = '/home/wittetam/CNIDetector/data/raw/'
+    fname = 'Run22_0000.h5'
+    with h5.File(dpath+fname, 'r') as f:
+        for key in f:
+            dark.append(np.array(f[key]))
+    dark = np.mean(np.array(dark), axis=0)
 # This function is called for every single event
 # following the given recipe of analysis
 def onEvent(evt):
@@ -46,22 +48,30 @@ def onEvent(evt):
     sys.stdout.flush()
     my_data = evt['photonPixelDetectors'][name].data
     if data_sum is None:
-        data_sum = my_data-dark
+        if do_dark_correction:
+            data_sum = np.copy(my_data-dark)
+        else:
+            data_sum = np.copy(my_data)
     else:
-        data_sum += my_data - dark
+        if do_dark_correction:
+            data_sum += my_data - dark
+        else:
+            data_sum += my_data
     
     # Visualize detector image(s)
     for key in evt['photonPixelDetectors'].keys():
+        print(key)
         plotting.image.plotImage(evt['photonPixelDetectors'][key], send_rate=10, group='Raw')
 
-    # "Dark" correction
-    img_dark = evt['photonPixelDetectors'][name].data - dark
-    add_record(evt["analysis"], "analysis", "dark_corrected", img_dark)
-    plotting.image.plotImage(evt['analysis']['dark_corrected'], group='Corrected')
+    if do_dark_correction:
+        # "Dark" correction
+        img_dark = evt['photonPixelDetectors'][name].data - dark
+        add_record(evt["analysis"], "analysis", "dark_corrected", img_dark)
+        plotting.image.plotImage(evt['analysis']['dark_corrected'], group='Corrected')
 
-    #Accumulated image
-    add_record(evt['analysis'], 'analysis', 'accumulated', data_sum)
-    plotting.image.plotImage(evt['analysis']['accumulated'], group='Corrected')
+        #Accumulated image
+        add_record(evt['analysis'], 'analysis', 'accumulated', data_sum)
+        plotting.image.plotImage(evt['analysis']['accumulated'], group='Corrected')
 
     # RMS value
     rms = evt['photonPixelDetectors'][name].data.std()
