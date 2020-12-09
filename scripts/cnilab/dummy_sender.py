ae6bbe7d5a51fd86d2e2fb6b648f47d19a301d51
Kartik Ayyer <kartik.ayyer@desy.de>
2020-12-09 15:34:08 +0100

    dummy_sender: Added socket options and made data bigger

--- scripts/cnilab/dummy_sender.py
+++ scripts/cnilab/dummy_sender.py
@@ -10,10 +10,12 @@ port = 5678
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind('tcp://*:%d' % port)
+socket.setsockopt(zmq.SNDHWM, 2)
+socket.setsockopt(zmq.SNDBUF, 100*1024**2)
 
 while True:
     evt = {}
-    evt['data'] = np.random.normal(0, 0.5, (200, 200))
+    evt['data'] = np.pad(np.random.normal(0, 0.5, (2000, 2000)).astype('f4'), ((20,), (80,)), mode='constant')
     evt['name'] = 'SIMCAM'
     evt['meta'] = {'pixel_size': 6.5e-6, 'gain': 1.0}
     
